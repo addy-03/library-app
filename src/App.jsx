e24c3329ad754c4794f6434f1c2193a1c81c1522
Aditya Wagh <adityawagh169@gmail.com>
2023-04-03 18:05:22 +0530

    Initialized app using npx C-R-A

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Library App</h1>
+      </header>
+      <main>
+        List of Available books
+        <div className="book-list">
+          <div className="book">
+            <img src="" alt="" className="cover-image" />
+            <div className="details">
+              
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
