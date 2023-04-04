8fb3d830732c605723daf446b4450fc140141446
Aditya Wagh <adityawagh169@gmail.com>
2023-04-04 16:08:59 +0530

    Added state to Login and Signup components

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,18 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-const Login = () => {
-  const [formData, setFormData] = useState({});
-  const handleLogin = () => {
+const Login = (e) => {
+  const [formData, setFormData] = useState({ email: "", password: "" });
+  const handleLogin = (e) => {
     console.log("formdata ", formData);
+    e.preventDefault();
   };
+
+  const navigate = useNavigate();
 
   return (
     <div className="login">
+      <form onSubmit={handleLogin}></form>
       <h2>Login</h2>
       <input
         type="email"
@@ -15,6 +20,7 @@ const Login = () => {
         id="email"
         placeholder="Email"
         required
+        value={formData.email}
         onChange={(e) =>
           setFormData((state) => {
             return { ...state, email: e.target.value };
@@ -27,15 +33,24 @@ const Login = () => {
         id="password"
         placeholder="Password"
         required
+        value={formData.password}
         onChange={(e) =>
           setFormData((state) => {
             return { ...state, password: e.target.value };
           })
         }
       />
-      <button onClick={handleLogin} className="login-btn">Login</button>
+      <button
+        onClick={(e) => handleLogin(e)}
+        className="login-btn"
+        type="submit"
+      >
+        Login
+      </button>
       <p>Don't have an account?</p>
-      <button className="register-btn">Register</button>
+      <button className="register-btn" onClick={() => navigate("/signup")}>
+        Register
+      </button>
     </div>
   );
 };
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,57 +1,71 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Signup = () => {
-  const [formData, setFormData] = useState({});
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
   const handleSignup = (e) => {
     e.preventDefault();
     console.log("formdata ", formData);
   };
 
+  const navigate = useNavigate();
+
   return (
-    <form className="signup">
-      <h2>Sign Up</h2>
-      <input
-        type="text"
-        name="Name"
-        id="name"
-        placeholder="Name"
-        required
-        onChange={(e) =>
-          setFormData((state) => {
-            return { ...state, name: e.target.value };
-          })
-        }
-      />
-      <input
-        type="email"
-        name="Email"
-        id="email"
-        placeholder="Email"
-        required
-        onChange={(e) =>
-          setFormData((state) => {
-            return { ...state, email: e.target.value };
-          })
-        }
-      />
-      <input
-        type="password"
-        name="Password"
-        id="password"
-        placeholder="Password"
-        required
-        onChange={(e) =>
-          setFormData((state) => {
-            return { ...state, password: e.target.value };
-          })
-        }
-      />
-      <button onClick={(e) => handleSignup(e)} type="submit">
-        Sign Up
-      </button>
+    <div className="signup">
+      <form>
+        <h2>Sign Up</h2>
+        <input
+          type="text"
+          name="Name"
+          id="name"
+          placeholder="Name"
+          required
+          value={formData.name}
+          onChange={(e) =>
+            setFormData((state) => {
+              return { ...state, name: e.target.value };
+            })
+          }
+        />
+        <input
+          type="email"
+          name="Email"
+          id="email"
+          placeholder="Email"
+          required
+          value={formData.email}
+          onChange={(e) =>
+            setFormData((state) => {
+              return { ...state, email: e.target.value };
+            })
+          }
+        />
+        <input
+          type="password"
+          name="Password"
+          id="password"
+          placeholder="Password"
+          required
+          value={formData.password}
+          onChange={(e) =>
+            setFormData((state) => {
+              return { ...state, password: e.target.value };
+            })
+          }
+        />
+        <button onClick={(e) => handleSignup(e)} type="submit">
+          Sign Up
+        </button>
+      </form>
       <p>Already have an account?</p>
-      <button className="login-btn">Login</button>
-    </form>
+      <button className="login-btn" onClick={() => navigate("/login")}>
+        Login
+      </button>
+    </div>
   );
 };
 
