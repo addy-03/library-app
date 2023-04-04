a77d7eca4ff82900801b02f06f8b492fd94a0d98
Aditya Wagh <adityawagh169@gmail.com>
2023-04-05 02:51:28 +0530

    Fixed BookModal

--- src/components/BookModal.jsx
+++ src/components/BookModal.jsx
@@ -4,12 +4,12 @@ import { BooksContext } from "../context/BooksContext";
 
 const BookModal = () => {
   const { data } = useContext(BooksContext);
-  // console.log("Modal data", data);
+  console.log("Modal data", data);
 
   const bookData = data?.books.filter(
     (book) => book.id === data.currentBookID
   )[0];
-  // console.log("Modal bookData", bookData);
+  console.log("Modal bookData", bookData);
 
   const closeModal = () => {
     let modalEl = document.getElementsByClassName("book-modal-container")[0];
@@ -17,28 +17,33 @@ const BookModal = () => {
   };
 
   return (
-    <div className="book-modal-container modal-hidden">
-      <div className="book-modal book">
-        <img src={bookData.data.image} alt="" className="cover-image" />
-        <div className="details">
-          <div className="title">
-            <b>Title:</b> {bookData.data.title}
+    <>
+      <div className="book-modal-container modal-hidden">
+        {bookData && (
+          <div className="book-modal book">
+            <img src={bookData.data.image} alt="" className="cover-image" />
+            <div className="details">
+              <div className="title">
+                <b>Title:</b> {bookData.data.title}
+              </div>
+              <div className="author">
+                <b>Author:</b> {bookData.data.author}
+              </div>
+              <div className="genres">
+                <b>Genres:</b> {bookData.data.genres?.join(", ")}
+              </div>
+              <div className="excerpt">
+                <b>Excerpt:</b> {bookData.data.excerpt}
+              </div>
+            </div>
           </div>
-          <div className="author">
-            <b>Author:</b> {bookData.data.author}
-          </div>
-          <div className="genres">
-            <b>Genres:</b> {bookData.data.genres?.join(", ")}
-          </div>
-          <div className="excerpt">
-            <b>Excerpt:</b> {bookData.data.excerpt}
-          </div>
-        </div>
+        )}
+        <span className="close-btn" onClick={closeModal}>
+          &times;
+        </span>
       </div>
-      <span className="close-btn" onClick={closeModal}>
-        &times;
-      </span>
-    </div>
+      {/* {!bookData && <div>No Data Available</div>} */}
+    </>
   );
 };
 
