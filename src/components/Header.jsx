93d88c011ce992988fcf1545dc9e9712eb8c1cd0
Aditya Wagh <adityawagh169@gmail.com>
2023-04-04 13:29:58 +0530

    Added Manage button to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ const Header = () => {
   return (
     <header>
       <h1>Library App</h1>
+      <button className="manage-btn">Manage</button>
       <div className="profile">
         <button>Login / SignUp</button>
         <div className="user">UserName</div>
